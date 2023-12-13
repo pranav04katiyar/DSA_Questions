2b9a567fa04304970e946d99cea8fb239491c8c3
pranav04katiyar <pranav04katiyar@gmail.com>
2023-12-13 21:00:40 +0530

    Added ArrayQuestions - RotateArraybyNElements

---
+++ src/Arrays_Questions/RotateArraybyNElements.java
@@ -0,0 +1,24 @@
+package Arrays_Questions;
+
+import java.lang.*;
+import java.util.*;
+public class RotateArraybyNElements {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();               //input the size
+        int[] A1 = new int[N];              //array creation
+        for(int i=0; i<N; i++){
+            A1[i] = sc.nextInt();           //input elements
+        }
+
+        int B = sc.nextInt();               //input the no. of rotations
+        int[] A2 = new int[N];              //create rotated arrays
+        for(int i=0; i<N; i++){
+            A2[(i+B)%N] = A1[i];            //input elements by rotating them
+        }
+
+        for(int i=0; i<N; i++){
+            System.out.print(A2[i]+" ");    //output elements
+        }
+    }
+}
