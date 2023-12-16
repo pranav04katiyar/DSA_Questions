dbe87e137ca52fa3b00a6887fd4da00c5b0e1016
pranav04katiyar <pranav04katiyar@gmail.com>
2023-12-16 11:44:34 +0530

    updated comment

--- src/Arrays_Questions/N_DividedBy_3_Repeat_number.java
+++ src/Arrays_Questions/N_DividedBy_3_Repeat_number.java
@@ -1,7 +1,6 @@
 package Arrays_Questions;
 
-/*You're given a read-only array of N integers.
-Find out if any integer occurs more than N/3 times in the array in linear time and constant additional space.
+/*You're given a read-only array of N integers. Find out if any integer occurs more than N/3 times in the array in linear time and constant additional space.
 If so, return the integer. If not, return -1.
 If there are multiple solutions, return any one.
 Note: Read-only array means that the input array should not be modified in the process of solving the problem.*/
