813add86d8cf597574ab2fce1a646edc451c8e2d
pranav04katiyar <pranav04katiyar@gmail.com>
2023-12-16 11:38:27 +0530

    Added question and explaination

--- src/Arrays_Questions/N_DividedBy_3_Repeat_number.java
+++ src/Arrays_Questions/N_DividedBy_3_Repeat_number.java
@@ -2,6 +2,15 @@ package Arrays_Questions;
 
 import java.util.Scanner;
 
+/*You're given a read-only array of N integers.
+Find out if any integer occurs more than N/3 times in the array in linear time and constant additional space.
+If so, return the integer. If not, return -1.
+If there are multiple solutions, return any one.
+Note: Read-only array means that the input array should not be modified in the process of solving the problem.*/
+
+/*This code aims to find the majority element(s) in an array.
+The majority element is defined as an element that appears more than ⌊n/3⌋ times in the array, where n is the size of the array.*/
+
 public class N_DividedBy_3_Repeat_number {
     public int repeatedNumber(int[] A) {
         int N = A.length;   //size of the array
