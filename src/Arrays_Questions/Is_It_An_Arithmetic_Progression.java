31e5b1d9a64462faf6ae311889ae3c63312459c1
pranav04katiyar <pranav04katiyar@gmail.com>
2023-12-16 12:15:16 +0530

    Added question commit

--- src/Arrays_Questions/Is_It_An_Arithmetic_Progression.java
+++ src/Arrays_Questions/Is_It_An_Arithmetic_Progression.java
@@ -1,5 +1,8 @@
 package Arrays_Questions;
 
+/* Given an integer array A of size N. Return 1 if the array can be arranged to form an arithmetic progression, otherwise return 0.
+A sequence of numbers is called an arithmetic progression if the difference between any two consecutive elements is the same.*/
+
 import java.util.Arrays;
 import java.util.Scanner;
 
