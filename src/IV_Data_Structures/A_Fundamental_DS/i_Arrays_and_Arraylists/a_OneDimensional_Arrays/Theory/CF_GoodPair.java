1858a48467fe0d38b47a2fdccf0913aa0e6954ae
pranav04katiyar <pranav04katiyar@gmail.com>
2024-02-24 20:56:53 +0530

    Updated good pair example for carry forward

--- src/IV_Data_Structures/A_Fundamental_DS/i_Arrays_and_Arraylists/a_OneDimensional_Arrays/Theory/CF_GoodPair.java
+++ src/IV_Data_Structures/A_Fundamental_DS/i_Arrays_and_Arraylists/a_OneDimensional_Arrays/Theory/CF_GoodPair.java
@@ -1,5 +1,7 @@
 package IV_Data_Structures.A_Fundamental_DS.i_Arrays_and_Arraylists.a_OneDimensional_Arrays.Theory;
 
+import java.util.Scanner;
+
 /*
 Questions:
 Given a character array s[N], we need to find the number of good pairs of indices (i, j) such that i < j and s[i] == 'a' and s[j] == 'g'.
@@ -8,17 +10,19 @@ All characters are lowercase English alphabets.
 public class CF_GoodPair {
 //Approach 1
     public static void main(String[] args) {
-        char[] s = {'a', 'g', 'a', 'g', 'a', 'g', 'a', 'g', 'a', 'g'};
+        Scanner sc = new Scanner(System.in);
+        char[] s = sc.next().toCharArray();             //Allows users to input char arrays
         int a_count = 0;
         int g_count = 0;
-        for(int i = s.length - 1; i>=0; i--){
-            if(s[i]=='g'){
-                g_count++;
+        for(int i = s.length - 1; i>=0; i--){               //Start the loop from the end
+            if(s[i]=='g'){                  //if and else if condition allows to ensure the loop is executing only when needed, saves time and space
+                g_count++;                          //if the loop finds a 'g' at any index, increase the g_count
             }
             else if(s[i]=='a'){
-                a_count = a_count + g_count;
+                a_count = a_count + g_count;        //if 'a' is found at any index, calculate how many pair can be made with the already stored 'g's
             }
         }
-        System.out.println(a_count);
+        if(a_count!=0) System.out.println(a_count);            //print the total no. of good pairs
+        else System.out.println("No good pairs found");
     }
 }
